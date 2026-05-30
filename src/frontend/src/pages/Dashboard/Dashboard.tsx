8616e69b89c9c3cc15b62c67389ca9a8297c9b0e
Felipej3ds <felipej.silvadf0@gmail.com>
2026-05-30 15:42:57 -0300

    salvando alterações no gráfico

--- src/frontend/src/pages/Dashboard/Dashboard.tsx
+++ src/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,18 @@
-import { Card } from '../../components/Card'
+import { Card } from '../../components/Card';
+import { TemperatureChart } from '../../components/Charts/TemperatureChart';
 
 export function Dashboard() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
+  return (
+    <div>
+      <h1>Dashboard</h1>
 
-            <Card
-                icon="analytics"
-                label="Total de usuários"
-                value="1.240"
-            />
-        </div>
-    )
+      <Card
+        icon="analytics"
+        label="Total de usuários"
+        value="1.240"
+      />
+
+      <TemperatureChart />
+    </div>
+  );
 }
