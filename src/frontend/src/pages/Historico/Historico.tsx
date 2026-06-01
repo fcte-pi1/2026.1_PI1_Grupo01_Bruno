a1aa7e9090c322d3e3e4213e8da65c69614b70af
isaqzin <isaquereserva@gmail.com>
2026-06-01 14:11:59 -0300

    fix: arruma o componente maze

--- src/frontend/src/pages/Historico/Historico.tsx
+++ src/frontend/src/pages/Historico/Historico.tsx
@@ -8,8 +8,16 @@ export function Historico() {
 
             <ControlBtn
             />
-
-            <Maze />
+            
+            <Maze  size={4} updates={ [
+                { index: 0, walls: { left: true, right: false, top: true, bottom: true } },
+                {index: 1, walls: { left: false, right: false, top: true, bottom: false} },
+                {index: 5, walls: { left: true, right: false, top: false, bottom: false } },
+                {index: 9, walls: { left: true, right: false, top: false, bottom: true} },
+                {index: 10, walls: { left: false, right: true, top: false, bottom: true} },
+                {index: 6, walls: { left: false, right: true, top: true, bottom: false } }
+            ]}
+            path={[0, 1, 5, 9, 10, 6]}/>
 
             <br />
 
