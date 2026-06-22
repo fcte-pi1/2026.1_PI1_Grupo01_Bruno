79df7436e62833971001abf225a8b221cac0fda1
Laisczt <laissopaes@gmail.com>
2026-06-22 13:49:13 -0300

    feat: duracao, vel média, e distância no histórico

--- src/frontend/src/pages/Historico/Historico.tsx
+++ src/frontend/src/pages/Historico/Historico.tsx
@@ -25,7 +25,7 @@ const columns: Column<any>[] = [
             return <Badge size='sm' type={type} label={label} />;
         }
     },
-    { key: 'duracao', label: 'Duração', icon: 'timer', render: (value) => <p>{Number(value).toFixed(1)}s</p> },
+    { key: 'duracao', label: 'Duração', icon: 'timer', render: (value) => <p>{Number(value).toFixed(3)}s</p> },
     { key: 'velocity', label: 'Vel. Média', icon: 'speed', render: (value) => <p>{Number(value).toFixed(2)} m/s</p> },
     { key: 'consume', label: 'Consumo', icon: 'electric_bolt', render: (value) => <p>{Number(value).toFixed(0)} mAh</p> },
     { key: 'distance', label: 'Distância', icon: 'route', render: (value) => <p>{Number(value).toFixed(2)} m</p> },
@@ -42,6 +42,26 @@ export function Historico() {
 
                 const listaCorridas = Object.entries(dadosNode);
                 const formatoTabela = listaCorridas.map(([firebaseId, corrida]: any, index) => {
+                    let distTotal = 0;
+                    const duration =  corrida.telemetria ? (safeNum(corrida.metadados.fim_timestamp) - safeNum(corrida.metadados.inicio_timestamp)) / 1000: null;
+                    let lastTimeStamp = 0;
+                    corrida.telemetria ? Object.values(corrida.telemetria).forEach( (tel) =>{
+                        let Tel = Object(tel)
+                        if(distTotal !== -1){
+                            if(lastTimeStamp === 0) {
+                                lastTimeStamp = safeNum(corrida.metadados.inicio_timestamp)
+                            }
+
+                            if(Tel.timestamp < lastTimeStamp){
+                                distTotal = -1
+                            }else{
+                                console.log(safeNum(Tel.velocidade) * (safeNum(Tel.timestamp - lastTimeStamp)) / 1000)
+                                distTotal += safeNum(Tel.velocidade) * (safeNum(Tel.timestamp) - lastTimeStamp) / 1000;
+                                // distTotal += (safeNum(Tel.velMedia) * (safeNum(Tel.timestamp) - safeNum(lastTimeStamp)));
+                                lastTimeStamp = safeNum(Tel.timestamp);
+                            }
+                        }
+                    }) as any : null;
                     const ultimaTel = corrida.telemetria ? Object.values(corrida.telemetria).pop() as any : null;
                     const mahRestante = safeNum(ultimaTel?.mah_restante);
                     return {
@@ -50,10 +70,10 @@ export function Historico() {
                         datetime: new Date(corrida.metadados?.inicio_timestamp || Date.now()).toLocaleString('pt-BR', { dateStyle: 'short', timeStyle: 'short' }),
                         size: `${corrida.metadados?.dimensao_labirinto || 16}x${corrida.metadados?.dimensao_labirinto || 16}` as any,
                         status: corrida.metadados?.status || 'concluido',
-                        duracao: safeNum(ultimaTel?.tempoMedio),
-                        velocity: safeNum(ultimaTel?.velMedia),
+                        duracao: duration !== null && duration > 0 ? duration : 0,
+                        velocity: duration !== null && duration > 0 && distTotal !== -1? distTotal / duration : 0,
                         consume: mahRestante > 0 ? 1000 - mahRestante : 0, 
-                        distance: safeNum(ultimaTel?.distancia)
+                        distance: distTotal !== -1? distTotal : 0
                     };
                 });
                 setData(formatoTabela.reverse()); 
